9e8936a9ebca57d75fdadefd65dc32f1d70a24ca
alfredbirk <alfred_birk@hotmail.com>
2015-03-06 09:17:00 +0100

    Add client/server

---
+++ CalendarServer/src/server/ServerProtocol.java
@@ -0,0 +1,16 @@
+package server;
+
+public class ServerProtocol {
+
+	public String processInput(String rawInput) throws Exception{
+		if(rawInput != null){
+			System.out.println(rawInput);
+			String[] input = rawInput.split(" ");
+			
+		}
+		
+		return "OK";
+		
+	}
+	
+}
