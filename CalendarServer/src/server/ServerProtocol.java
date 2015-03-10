68bb59497d3903ac794318df1cfbcb00fedcf712
alfredbirk <alfred_birk@hotmail.com>
2015-03-10 10:26:23 +0100

    Changed addNewAppointment in server

--- CalendarServer/src/server/ServerProtocol.java
+++ CalendarServer/src/server/ServerProtocol.java
@@ -34,14 +34,21 @@ public class ServerProtocol {
 				return response;
 			
 			case "ADDNEWAPOINTMENT":
-				//stuff here
-				return "";
+				
+				String description = input[1];
+				String startTime = input[2];
+				String endTime = input[3];
+				String location = input[4];
+				String meetingRoom = input[5];
+				String owner = input[6];
+				
+				String respone = database.addNewAppointment(description, startTime, endTime, location, meetingRoom, owner);
+				return response;
 				
 			case "DELETEAPPOINTMENT":
 				
 				String response = database.removeAppointment(input[1]);
 				return response;
-				return "";
 				
 			}
 		}
