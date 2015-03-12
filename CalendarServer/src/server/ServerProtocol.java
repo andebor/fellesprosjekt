61ff541531d12dfa5154f04e24a842ef9c1c8019
andebor <post@andersborud.com>
2015-03-12 14:05:26 +0100

    Added methods for setting new password per user.

--- CalendarServer/src/server/ServerProtocol.java
+++ CalendarServer/src/server/ServerProtocol.java
@@ -123,6 +123,18 @@ public class ServerProtocol {
 				String appID = input[2];
 				Boolean response5 = database.addEmployeeToAppointment(Integer.parseInt(appID), Integer.parseInt(empNo));
 				return response5.toString();
+				
+			case "SETNEWPASSWORD":
+				String UserName = input[1];
+				String newpassword = input[2];
+				
+				byte[] newSalt = Security.generateSalt();
+				char[] newpwd = newpassword.toCharArray();
+				byte[] hashedNewPwd = Security.hashPassword(newpwd, newSalt);
+				String encodedNewPwd = Security.bytetoString(hashedNewPwd);
+				
+				String changeResponse = database.setNewPass(UserName, encodedNewPwd, newSalt);
+				return changeResponse;
 			
 			} //Closing bracket for switch statement
 
