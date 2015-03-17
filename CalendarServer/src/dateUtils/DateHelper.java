6ac0d62dbbcc923e588f2d1e8695170e9dac5c37
Bang <mariusmbang@gmail.com>
2015-03-17 08:29:23 +0100

    getNorwegianFormat() added

--- CalendarServer/src/dateUtils/DateHelper.java
+++ CalendarServer/src/dateUtils/DateHelper.java
@@ -4,6 +4,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.Locale;
 import java.util.TimeZone;
 
 public class DateHelper {
@@ -15,8 +16,56 @@ public class DateHelper {
 		return time;
 	}
 	
+	public static String getMySQLDatetime(Date date) {
+		//TODO: Fill out if necessary
+		return null;
+	}
+	
+	public static long getDateLong(String mysqlDT) {
+		String [] date = mysqlDT.split("-");
+		int year = Integer.parseInt(date[0]);
+		int month = Integer.parseInt(date[1]);
+		int day = Integer.parseInt(date[2].substring(0, 2));
+		String time = date[2].substring(3, 3+5);
+		int hour = Integer.parseInt(time.substring(0, 2));
+		int minutes = Integer.parseInt(time.substring(3, 3+2));
+		TimeZone timezone = TimeZone.getDefault();
+		Calendar calendar = new GregorianCalendar(timezone);
+		calendar.set(year, month - 1, day, hour, minutes);
+		return calendar.getTimeInMillis();
+	}
+	
+	public static String getMySQLDateTime(String mysqlDT, int offset) {
+		//Fungerer ikke som den skal
+		TimeZone timezone = TimeZone.getDefault();
+		Calendar calendar = new GregorianCalendar(timezone);
+		long oldTime = DateHelper.getDateLong(mysqlDT);
+		long newTime = oldTime + offset*60*1000;
+		calendar.setTimeInMillis(newTime);
+		SimpleDateFormat timeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		timeFormat.setTimeZone(TimeZone.getTimeZone("GMT+1"));
+		String time = timeFormat.format(calendar.getTime());
+		return time;
+	}
+	
+	public static String getNorwegianFormat(String mysqlDT) {
+		TimeZone timezone = TimeZone.getDefault();
+		Calendar calendar = new GregorianCalendar(timezone);
+		String [] date = mysqlDT.split("-");
+		int year = Integer.parseInt(date[0]);
+		int month = Integer.parseInt(date[1]);
+		int day = Integer.parseInt(date[2].substring(0, 2));
+		String time = date[2].substring(3, 3+5);
+		int hour = Integer.parseInt(time.substring(0, 2));
+		int minutes = Integer.parseInt(time.substring(3, 3+2));
+		calendar.set(year, month - 1, day, hour, minutes);
+		String dayName = calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.getDefault());
+		String weekday = DateHelper.getDayInNorwegian(dayName);
+		String monthName = DateHelper.getMonthInNorwegian(month - 1);
+		return weekday + " " + day + ". " + monthName + " klokken " + hour + ":" + minutes;
+	}
+	
 	public static String getDayInNorwegian(int dayNo) {
-		//TODO: Flytt denne metoden et egnet sted
 		switch (dayNo) {
 		case 0: return "mandag";
 		case 1: return "tirsdag";
@@ -32,7 +81,6 @@ public class DateHelper {
 	}
 	
 	public static String getDayInNorwegian(String dayName) {
-		//TODO: Flytt denne metoden et egnet sted
 		switch (dayName) {
 		case "Mon": return "mandag";
 		case "Tue": return "tirsdag";
@@ -71,7 +119,8 @@ public class DateHelper {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		DateHelper dh = new DateHelper();
-		System.out.println(dh.getMySQLDateTime());
+		//System.out.println(dh.getMySQLDateTime());
+		System.out.println(dh.getMySQLDateTime("2015-09-11 13:45:00", 0));
 	}
 
 }
